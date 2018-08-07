98dc8520c22c291987016244b2c6b85ad0319676
Jeffery Patton <jefferypatton@Jeff-Pattons-MacBook-Pro.local>
2018-08-07 13:06:21 -0400

    Prelunch + lab

---
+++ instructions.js
@@ -0,0 +1,15 @@
+/*
+  Create a react application the sends GET requests to the jsonplaceholder API. 
+
+  You application should have two inputs: one for the route to GET from(users, posts, comments, etc), and another for how many items to get from the API. If the user inputs 0, do not put a number on the end of the request. 
+
+  https://jsonplaceholder.typicode.com/
+
+  After you have received the data, create output components that properly format the data you have received. So for the 'users' route output all of the users that you received from the API, with a format like this:
+  Name: (first-level item)
+  Username: (first-level item)
+  Email: (first-level item)
+  Address: (format this from the address object)
+
+
+*/
